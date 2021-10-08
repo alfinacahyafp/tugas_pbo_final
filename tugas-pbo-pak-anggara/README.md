16ddf7ed1408075976a529c5aa425a6d6f38deff
alfina cahya <85860167+alfinacahyafp@users.noreply.github.com>
2021-10-08 13:59:18 +0700

    pertemuan 8

---
+++ tugas-pbo-pak-anggara/README.md
@@ -0,0 +1,1 @@
+# tugas-pbo-pak-anggara
---
+++ tugas-pbo-pak-anggara/Tugas-PBO-XII-RPL-2/tugasmvc-pertemuan8/Views/Home.php
@@ -0,0 +1,33 @@
+<html>
+
+<head>
+    <title>Aplikasi Pembayaran</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <style>
+        body {
+            background-color: pink;
+            height: auto;
+        }
+
+        h1 {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>NAMA : ALFINA CAHYA F. P</h1>
+        <h1>KELAS : XII RPL 2</h1>
+        <div class="position-absolute top-50 start-50 translate-middle">
+            <img src="../ALFINA.png" alt="">
+
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
